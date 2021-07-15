3e21fe37997f2989f0f9d749b4bf11643f98cad0
Lucas Limeira <lucasalveslm@gmail.com>
2021-07-14 21:51:23 -0300

    added interactive setup

--- setup.sh
+++ setup.sh
@@ -3,20 +3,43 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ ! -f $HOME/.tmux.conf ]; then
-    echo "Setup tmux config"
-    ln -s $SCRIPTPATH/tmux/tmuxconf $HOME/.tmux.conf
-fi
+function src_shell() {
+    echo
+    read -p "Do you want to reload $SHELL? (y/N) "
+    if [[ $REPLY =~ [Yy]$ ]]; then
+        exec $SHELL
+    fi
+}
 
-if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-    echo "Setup tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-fi
+function _setup() {
+    local EXP="[ $1 $2 ]"
+    if eval $EXP; then 
+        echo
+        read -p "$2 exists. Want to override? (y/N) "
+        if [[ ! $REPLY =~ [Yy]$ ]]; then
+            echo "$2 skipped"
+            return 1
+        fi
+        echo "Overriding $2"
+        mv $2 "$2.backup"
+    fi
+    echo "$2 setup!"
+}
 
-if [ ! -f $HOME/.zshrc ]; then
-    echo "Setup zsh config"
-    ln -s $SCRIPTPATH/zsh/zshrc $HOME/.zshrc
+function setup() {
+    if _setup $1 $2; then
+        ln -s $3 $2
+    fi
+}
+function setup_git() {
+    if _setup "-d" $1; then
+        git clone $2 $1
+    fi
+}
 
-    rm -f $HOME/.zprofile
-    echo 'source $HOME/.zshrc' >> $HOME/.zprofile
-fi
+
+setup -f $HOME/.tmux.conf $SCRIPTPATH/tmux/tmuxconf
+setup -f $HOME/.zshrc $SCRIPTPATH/zsh/zshrc
+setup_git $HOME/.tmux/plugins/tpm https://github.com/tmux-plugins/tpm
+
+src_shell
